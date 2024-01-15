0418f900fcf97bca41c650fda9758ac39280fbba
Biplob Das <biplob90.d@gmail.com>
2024-01-15 11:39:32 +0200

    My first Sevlte project

---
+++ src/components/Counter.svelte
@@ -0,0 +1,22 @@
+<script>
+    let count = $state({ 
+      value: 0,
+      note: "single digits",
+    });
+  
+    // setInterval(() => {
+    //   count.value++;
+    //   if (count.value > 9) {
+    //   count.note = "not single digits";
+    // }
+    // }, 1000);
+
+    const incrementCount = () => {
+    count.value++;
+    if (count.value > 9) {
+      count.note = "not single digits";
+    }
+  };
+  </script>
+  <button on:click={incrementCount}>Increment</button>
+  <p>Count: {count.value} ({count.note})</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Counter from "../components/Counter.svelte";
+    import Hello from '../components/Hello.svelte';
+    import QuestionAndAnswer from '../components/QuestionAndAnswer.svelte';
+    let qa = {
+    question: "Can this work?",
+    answer: "Yes it can!",
+  };
+</script>
+  
+
+
+<Hello />
+<Counter />
+<QuestionAndAnswer {...qa} />
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  
